4ba70ec73750bae610478fe1d6df8fbe63abfbda
adnan shaikh <adnn4u@gmail.com>
2023-10-16 14:22:41 +0530

    tools part done in ui

--- src/app/pages/home/tools/tools.tsx
+++ src/app/pages/home/tools/tools.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import "./tools.css"
+import Image from "next/image"
+import t1 from "./img/t1.png"
+import t2 from "./img/t2.png"
+import t3 from "./img/t3.png"
+import t4 from "./img/t4.png"
+
 import { HiArrowSmallRight } from 'react-icons/hi2';
 import { HiArrowSmallLeft } from 'react-icons/hi2';
 const Tools = () => {
@@ -8,19 +14,73 @@ const Tools = () => {
       <div className="tools-area">
         <div className="tool-heading">
             <div className="tool-txt">
-                <h4>See you later 16
+                <h4>See you later 16 <br />
 different HR tools</h4>
 <p>We’re simplifying every aspect of managing a worldwide team, from benefits and
 equity to working visas and equipment. It’s one platform made to get you set up
 compliantly in just 5 minutes.</p>
-            </div>
-            <div className="tool-arr">
-                <HiArrowSmallRight/>
-                <HiArrowSmallLeft/>
-            </div>
         </div>
-      </div>
+        <div className="tool-arr">  
+            <HiArrowSmallLeft className="left-arr"/>
+            <HiArrowSmallRight/>
+        
+        </div>
     </div>
+    <div className="ser-list">
+        <div className="ser-item">
+        <Image
+    src={t1}
+    alt=''
+    />
+    <h3>Use Deel HR free to
+manage your global
+team in one system</h3>
+<div className="learn-more">
+    <p>Learn More     <HiArrowSmallRight/> </p>
+</div>
+        </div>
+        <div className="ser-item" style={{backgroundColor:"#519AF4"}}>
+        <Image
+    src={t2}
+    alt=''
+    />
+    <h3>Hire or relocate team
+members with in-
+house visa support</h3>
+<div className="learn-more">
+    <p>Learn More     <HiArrowSmallRight/> </p>
+</div>
+        </div>
+        <div className="ser-item" style={{backgroundColor:"#1C4396"}}>
+        <Image
+    src={t3}
+    alt=''
+    />
+    <h3>Use Deel HR free to
+manage your global
+team in one system</h3>
+<div className="learn-more">
+    <p>Learn More     <HiArrowSmallRight/> </p>
+</div>
+        </div>
+        <div className="ser-item" style={{backgroundColor:"#F78400"}} >
+        <Image
+    src={t4}
+    alt=''
+    />
+    <h3>Use Deel HR free to
+manage your global
+team in one system</h3>
+<div className="learn-more">
+    <p>Learn More     <HiArrowSmallRight/> </p>
+</div>
+        </div>
+    </div>
+    <div className="end-demo-btn">
+    <button className='demo-btn'>Get A Demo</button>
+    </div>
+    </div>
+</div>
   )
 }
 
