3f7a1873b3979050441c08fe9ec114abda339921
adnan shaikh <adnn4u@gmail.com>
2023-10-18 11:19:13 +0530

    home page done

--- src/app/pages/home/country/country.tsx
+++ src/app/pages/home/country/country.tsx
@@ -1,25 +1,57 @@
 import React from 'react'
 import c1 from "./img/c1.png"
-import sa from "./img/sa.png"
+import c2 from "./img/c2.png"
+import c3 from "./img/c3.png"
+import ind from "./img/ind.png"
+import pak from "./img/pak.png"
 import Image from 'next/image'
 import "./country.css"
 const Country = () => {
   return (
     <div className='country'>
-      <div className="image-area">
-        <div className="img-div" >
-<Image src={c1} alt=''/>
-<div className="overlay-txt">
-<p>South Africa</p>
-<div className="c-i-area">
-  <div className="bottom-part">
-  <span>EOR</span>
-  <span>Contractor</span>
-</div>
-<Image src={sa} alt='' className='flag-img'/>
-</div>
-</div>
+    <div className="image-area">
+      <div className="img-div" >
+  <Image src={c1} alt=''/>
+  <div className="overlay-txt">
+  <p>South Africa</p>
+  <div className="c-i-area">
+    <div className="bottom-part">
+    <span>EOR</span>
+    <span>Contractor</span>
+  </div>
+  <Image src={ind} alt='' className='flag-img'/>
+  </div>
+  </div>
       </div>
+
+      <div className="img-div-type-2 img-div " >
+  <Image src={c2} alt=''/>
+  <div className="overlay-txt">
+  <p>South Africa</p>
+  <div className="c-i-area">
+    <div className="bottom-part">
+    <span>EOR</span>
+    <span>Contractor</span>
+  </div>
+  <Image src={pak} alt='' className='flag-img'/>
+  </div>
+  </div>
+      </div>
+
+      <div className="img-div" >
+  <Image src={c3} alt=''/>
+  <div className="overlay-txt">
+  <p>South Africa</p>
+  <div className="c-i-area">
+    <div className="bottom-part">
+    <span>EOR</span>
+    <span>Contractor</span>
+  </div>
+  <Image src={ind} alt='' className='flag-img'/>
+  </div>
+  </div>
+      </div>
+
       </div>
     </div>
   )
