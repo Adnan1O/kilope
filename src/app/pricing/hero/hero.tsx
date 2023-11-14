1dd870746714fbb9d6d60ddde3de579d2da641b8
adnan shaikh <adnn4u@gmail.com>
2023-11-14 11:05:50 +0530

    navbar error

--- src/app/pricing/hero/hero.tsx
+++ src/app/pricing/hero/hero.tsx
@@ -6,7 +6,8 @@ import { BiMenuAltRight } from 'react-icons/bi';
 import Resnav from '../../comps/resnav/resnav';
 import "./hero.css"
 import { BsArrowRightShort } from 'react-icons/bs';
-// import Navbar from '../../comps/navbar/navbar';
+import Navbar1 from "../../comps/Navbar/Navbar"
+
 const Hero = () => {
   const [openNav, setOpenNav] = useState(false);
 
@@ -20,7 +21,7 @@ const Hero = () => {
     <p>✨ Limited time offer. EOR in <span>india, Indonesia & Nigeria </span> at <span>$169</span> in <span>Portugal, UK & Serbia</span> at <span>$229</span> . Valid for new customers! ✨</p>
   <button>Explore more countries <BsArrowRightShort/></button>
   </div>
- {/* <Navbar/> */}
+ <Navbar1/>
 <div className="hire">
   <h3>Hire and expand globally,</h3>
   <h3>at a fraction of the cost</h3>
